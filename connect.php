21af4d9f1531e6fb0967599f756d1663eedbec84
Agaba916 <agabahappy7@gmail.com>
2026-03-12 21:22:23 +0200

    Add connection to database

---
+++ connect.php
@@ -0,0 +1,18 @@
+<?php
+
+
+$env = parse_ini_file(".env");
+
+$servername = $env['DB_HOST'];
+$username   = $env['DB_USER'];
+$password   = $env['DB_PASS'];
+$dbname     = $env['DB_NAME'];
+
+$conn = new mysqli($servername,$username,$password,$dbname);
+
+if($conn->connect_error){
+    exit('Connection failed: '.$conn->connect_error);
+}
+
+echo 'Successfully connected!';
+?>
